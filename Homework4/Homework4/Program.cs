104f647fd8ff1b08a08527b2e6be23a6a9bc449e
andrii-bublyk <andriy.bublyk1991@gmail.com>
2018-11-27 10:18:53 +0200

    Homework04: - added courses xml import/export

--- Homework4/Homework4/Program.cs
+++ Homework4/Homework4/Program.cs
@@ -1,21 +1,45 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Homework4
 {
+    public static class ExtentionClass
+    {
+        public static string GetDate(this DateTime date)
+        {
+            return date.ToString("dd.MM.yyy", CultureInfo.InvariantCulture);
+        }
+
+        public static DateTime GetDate(this string date)
+        {
+            string[] splitDate = date.Split('.');
+            int year = int.Parse(splitDate[2]);
+            int month = int.Parse(splitDate[1]);
+            int day = int.Parse(splitDate[0]);
+
+            return new DateTime(year, month, day);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Academy academy = new Academy();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            //Course course1 = new Course(1, "course01", new DateTime(2018, 10, 5), new DateTime(2018, 12, 22), 80);
+            //academy.Courses.Add(course1);
+            //XDocument exportResult = academy.ExportToXml();
+            //exportResult.Save("academy.xml");
+
+            academy.ImportFromXml("academy.xml");
+
+            Console.ReadKey();
         }
     }
 }
